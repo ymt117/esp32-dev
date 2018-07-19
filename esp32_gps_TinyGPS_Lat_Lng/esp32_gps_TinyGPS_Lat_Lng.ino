6fb6916472b17dcd4a23c20f380f4779ed166a39
Yamato <k3402312@kadai.jp>
2018-07-19 20:06:20 +0900

    Add esp32_gps_TinyGPS

---
+++ esp32_gps_TinyGPS_Lat_Lng/esp32_gps_TinyGPS_Lat_Lng.ino
@@ -0,0 +1,26 @@
+// 参考：http://deviceplus.jp/hobby/entry060/
+
+#include <TinyGPS++.h>
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+HardwareSerial ss(0);
+
+void setup() {
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("GPS test start!"));
+}
+
+void loop() {
+  while(ss.available() > 0){
+    char c = ss.read();
+    gps.encode(c);
+    if(gps.location.isUpdated()){
+      Serial.print("Lat=\t");   Serial.print(gps.location.lat(), 6);
+      Serial.print("Lng=\t");   Serial.print(gps.location.lng(), 6);
+      Serial.print("Alt=\t");   Serial.println(gps.altitude.meters());
+    }
+  }
+
+}
